12078f934aaae881dd3a826e494f13eedc5dde92
Daron <82853275+daronphang@users.noreply.github.com>
2021-06-19 08:54:01 +0800

    Update reducer.ts

--- javascript/angular/NgRx/tutorial/reducer.ts
+++ javascript/angular/NgRx/tutorial/reducer.ts
@@ -20,3 +20,20 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
       return state;
   }
 }
+
+
+// app.reducer.ts:
+import { ActionReducerMap } from '@ngrx/store';
+
+import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
+import * as fromAuth from '../auth/store/auth.reducer';
+
+export interface AppState {
+  shoppingList: fromShoppingList.State;
+  auth: fromAuth.State;
+}
+
+export const appReducer: ActionReducerMap<AppState> = {
+  shoppingList: fromSHoppingList.shoppingListReducer,
+  auth: fromAuth.authReducer
+};
