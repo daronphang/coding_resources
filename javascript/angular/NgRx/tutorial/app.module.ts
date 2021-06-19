599a9b13b6069a1cc211c4be7e94d57d8a9d234f
Daron <82853275+daronphang@users.noreply.github.com>
2021-06-19 08:52:39 +0800

    Update app.module.ts

--- javascript/angular/NgRx/tutorial/app.module.ts
+++ javascript/angular/NgRx/tutorial/app.module.ts
@@ -8,32 +8,6 @@ imports: [
 
 
 
-// shopping-list.component.ts:
-
-export class ShoppingListComponent implements OnInit {
-  constructor(private store: Store<shoppingList: {ingredients: Ingredient[]}}> ) {}  // type is key chosen in app-module
-  
-  ingredients: Observable<{ingredients: Ingredient[] }>
-  
-  ngOnInit() {
-    this.ingredients = this.store.select('shoppingList')
-  }
-}
-
-
-```
-
-```javascript
-// shopping-edit.component.ts:
-import * as ShoppingListActions from './shopping-list.actions'; 
-
-onSubmit(form: NgForm) {
-  this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));    // dispatch actions
-}
-
-
-```
-
 ```html
 <li *ngFor="let ingredient of (ingredients | async).ingredients" >
 ```
