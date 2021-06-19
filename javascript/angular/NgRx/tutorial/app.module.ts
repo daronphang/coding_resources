7d469db0a4dd7cff5a1686766d213cb9d42b7b7d
Daron <82853275+daronphang@users.noreply.github.com>
2021-06-19 08:52:06 +0800

    Update app.module.ts

--- javascript/angular/NgRx/tutorial/app.module.ts
+++ javascript/angular/NgRx/tutorial/app.module.ts
@@ -7,21 +7,7 @@ imports: [
 ] 
 
 
-import { Action } from '@ngrx/store';
-import { Ingredient } from './shared/ingredient.model';
 
-
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-
-export class AddIngredient implements Action {
-  readonly type = ADD_INGREDIENT;
-  
-  constructor(public payload: Ingredient) {}
-}
-
-```
-
-```javascript
 // shopping-list.component.ts:
 
 export class ShoppingListComponent implements OnInit {
