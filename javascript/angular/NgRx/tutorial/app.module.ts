778e2f0ae91f1ef21c6cc3027bc4970c1c0774bd
Daron <82853275+daronphang@users.noreply.github.com>
2021-06-19 08:50:39 +0800

    Update app.module.ts

--- javascript/angular/NgRx/tutorial/app.module.ts
+++ javascript/angular/NgRx/tutorial/app.module.ts
@@ -5,35 +5,8 @@ imports: [
   StoreModule.forRoot(fromApp.appReducer),
   EffectsModule.forRoot([AuthEffects]);
 ] 
-```
 
-```javascript
-// shopping-list.reducer.ts
-// store both files in store folder
-import { Action } from '@ngrx/store';
-import { ADD_INGREDIENT } from './shopping-list.actions';
-import * as ShoppingListActions from './shopping-list.actions'; 
 
-const initialState = {
-  ingredients: ['apples', 'oranges']
-}
-
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
-  switch (action.type) {
-    case ShoppingListActions.ADD_INGREDIENT:
-      return {
-        ...state,   // copying existing state
-        ingredients: [...state.ingredients, action.payload]    // overwriting ingredients state
-      };
-      
-    default: 
-      return state;
-  }
-}
-```
-
-```javascript
-// shopping-list.actions.ts
 import { Action } from '@ngrx/store';
 import { Ingredient } from './shared/ingredient.model';
 
