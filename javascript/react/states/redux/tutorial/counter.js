a79fdf7f390daa4b07d4af85df7742cebdaefa5b
Daron <82853275+daronphang@users.noreply.github.com>
2021-06-26 12:44:08 +0800

    Update counter.js

--- javascript/react/states/redux/tutorial/counter.js
+++ javascript/react/states/redux/tutorial/counter.js
@@ -1,11 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
   // hook executed by React redux to retrieve part of state
   // automatically setup subscription to redux store for the current component
   const counter = useSelector(state => state.counter);  
   
+  const dispatch = useDispatch();
+  
+  const incrementHandler = () => {
+    dispatch({type: 'INCREMENT'});
+  }
+  
   return (
     <div>{counter}</div>
+    <button onClick={incrementHandler}>Increment</button>
   )
 }
