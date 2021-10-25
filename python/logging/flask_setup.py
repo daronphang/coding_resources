38521c2c24c596d9a64f53842b361cc01acc06de
Daron <82853275+daronphang@users.noreply.github.com>
2021-10-25 14:04:26 +0800

    Update flask_setup.py

--- python/logging/flask_setup.py
+++ python/logging/flask_setup.py
@@ -12,3 +12,25 @@ def blog():
     return f"Welcome to the Blog"
  
 app.run(host='localhost', debug=True)
+
+
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
