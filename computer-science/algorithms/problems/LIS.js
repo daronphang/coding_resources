97d5640d336dba1b9631a59b54cc28a0f47fe5fa
Daron <82853275+daronphang@users.noreply.github.com>
2022-04-14 22:32:45 +0800

    Update LIS.js

--- computer-science/algorithms/problems/LIS.js
+++ computer-science/algorithms/problems/LIS.js
@@ -1,4 +1,8 @@
 var lengthOfLIS = function (nums) {
+  // given an array [1,0,2,3,5,4]
+  // solve [1] first, followed by [1,0], then [1,0,2]
+  // each time you increment, can use results from previous iteration
+  
   const dp = Array(nums.length).fill(1);
 
   for (let i = 0; i < nums.length; i++) {
