e13e2f65c67078c2ba160c23399642cb16a64bfb
Daron <82853275+daronphang@users.noreply.github.com>
2022-04-20 01:07:01 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,86 +1,76 @@
-var wordBreak = function (s, wordDict) {
-  const dp = [];
-  const cutPos = [];
-  const results = [];
-  const firstWords = [];
+var numberOfArithmeticSlices = function (nums) {
+  // min of 3 elements, difference has to be equal
   const hashmap = {};
+  const hashmapZero = {};
+  let diff;
+  let temp;
+  let isNew = false;
 
-  let start = 0;
-  let s2 = "";
+  for (let i = 0; i < nums.length; i++) {
+    isNew = false;
+    for (let j = i + 1; j < nums.length; j++) {
+      diff = nums[j] - nums[i];
 
-  for (let i = 0; i < wordDict.length; i++) {
-    s2 += wordDict[i] + " ";
-    hashmap[wordDict[i]] = true;
-  }
-
-  for (let i = 0; i <= s2.length; i++) {
-    if (i === 0) dp[i] = Array(s.length + 1).fill(true);
-    else {
-      dp[i] = Array(s.length + 1);
-      dp[i][0] = false;
-    }
-  }
-
-  for (let i = 1; i < s2.length; i++) {
-    for (let j = 1; j <= s.length; j++) {
-      if (s2[i - 1] === " ") {
-        dp[i] = Array(s.length + 1).fill(true);
+      // diff of 0 is unique case
+      if (diff === 0) {
+        if (hashmapZero[nums[i]] !== undefined) {
+          if (!isNew) break;
+          hashmapZero[nums[i]] = hashmapZero[nums[i]] + 1;
+        } else {
+          hashmapZero[nums[i]] = 2;
+          isNew = true;
+        }
         continue;
       }
 
-      if (s2[i - 1] === s[j - 1] && dp[i - 1][j - 1]) {
-        dp[i][j] = true;
-
-        if (s2[i] === " ") {
-          if (j - (i - start) === 0) firstWords.push(j);
-          else cutPos.push(j);
-          start = i + 1;
-        }
+      if (hashmap[diff] === undefined) {
+        hashmap[diff] = [nums[j], 2];
       } else {
-        dp[i][j] = false;
-      }
-    }
-  }
-
-  // no starting matching words found in string
-  if (firstWords.length === 0) return [];
-
-  let sentence = "";
-  let duplicates = [];
-  let end;
-  let word;
-  let spaceCount;
-
-  cutPos.sort((a, b) => a - b);
-
-  for (let i = 0; i < firstWords.length; i++) {
-    sentence = s.substring(0, firstWords[i]) + " ";
-    end = firstWords[i];
-    spaceCount = 1;
-
-    for (let j = 0; j < cutPos.length; j++) {
-      if (end === cutPos[j]) {
-        duplicates.push(end);
-        continue;
-      }
-      word = s.substring(end, cutPos[j]);
-      if (!hashmap[word]) break;
-      sentence += word + " ";
-      end = cutPos[j];
-      spaceCount++;
-
-      if (j === cutPos.length - 1) {
-        if (sentence.length - spaceCount !== s.length) break;
-        else {
-          sentence = sentence.substring(0, sentence.length - 1);
-          results.push(sentence);
+        temp = hashmap[diff];
+        if (temp[0] === nums[i]) {
+          temp = [nums[j], temp[1] + 1];
+          hashmap[diff] = temp;
         }
       }
     }
   }
 
-  console.log(results);
+  let results = 0;
+
+  for (let key in hashmap) {
+    // each key represents the diff
+    // value contains number of arithmetic elements
+    temp = hashmap[key];
+    if (temp[1] < 3) continue;
+
+    results += calSeq(temp[1], false);
+  }
+
+  for (let key in hashmapZero) {
+    temp = hashmapZero[key];
+    if (temp < 3) continue;
+
+    results += calSeq(temp, true);
+  }
+
+  function calSeq(num, isZero) {
+    let total = 1;
+    let val = 2;
+    let zeroTotal = 1;
+
+    while (num > 3) {
+      total += val;
+      val++;
+      num--;
+
+      zeroTotal = zeroTotal * 2 + total;
+    }
+
+    if (isZero) return zeroTotal;
+    else return total;
+  }
+
   return results;
 };
 
-wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"]);
+console.log(numberOfArithmeticSlices([2, 4, 6, 2, 8, 2, 3, 10, 2, 2, 2, 1, 0]));
