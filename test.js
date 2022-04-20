7c6e7386f58c4110ffa8346ab2391565a16074cb
Daron <82853275+daronphang@users.noreply.github.com>
2022-04-21 07:55:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,76 +1,50 @@
-var numberOfArithmeticSlices = function (nums) {
-  // min of 3 elements, difference has to be equal
-  const hashmap = {};
-  const hashmapZero = {};
-  let diff;
-  let temp;
-  let isNew = false;
+var minCut = function (s) {
+  // return min. number of cuts
+  // row is s.length as there can be palindrome in latter portion
+  // i.e. tehannahablol
+  let minCuts = s.length - 1;
+  // const row = Math.ceil(s.length/2);
 
-  for (let i = 0; i < nums.length; i++) {
-    isNew = false;
-    for (let j = i + 1; j < nums.length; j++) {
-      diff = nums[j] - nums[i];
+  const dp = [];
+  let closest;
+  let skip = 0;
 
-      // diff of 0 is unique case
-      if (diff === 0) {
-        if (hashmapZero[nums[i]] !== undefined) {
-          if (!isNew) break;
-          hashmapZero[nums[i]] = hashmapZero[nums[i]] + 1;
-        } else {
-          hashmapZero[nums[i]] = 2;
-          isNew = true;
-        }
-        continue;
+  for (let i = 0; i <= s.length; i++) {
+    dp[i] = Array(s.length + 2).fill(0);
+  }
+
+  for (let i = 1; i <= s.length; i++) {
+    if (skip) {
+      skip--;
+      continue;
+    }
+    for (let j = s.length + 1; j > i; j--) {
+      if (s[i - 1] === s[j - 1]) {
+        closest = j;
+        dp[i][i] = 1;
       }
+      if (j === i + 1) {
+        if (dp[i - 1][j - 2] && dp[i - 1][j]) {
+          // for odd number palindrome
+          minCuts -= dp[i - 1][j] * 2;
 
-      if (hashmap[diff] === undefined) {
-        hashmap[diff] = [nums[j], 2];
-      } else {
-        temp = hashmap[diff];
-        if (temp[0] === nums[i]) {
-          temp = [nums[j], temp[1] + 1];
-          hashmap[diff] = temp;
+          // to skip to last charc of palindrome
+          skip = dp[i - 1][j] - 1;
+        } else if (closest) {
+          // even number palindrome
+          dp[i][closest] = dp[i - 1][closest + 1] + 1;
+          closest = 0;
+
+          if (dp[i][j - 1] && dp[i][j]) {
+            minCuts -= dp[i][j] * 2 - 1;
+            skip = dp[i][j] - 1;
+          }
         }
       }
     }
   }
 
-  let results = 0;
-
-  for (let key in hashmap) {
-    // each key represents the diff
-    // value contains number of arithmetic elements
-    temp = hashmap[key];
-    if (temp[1] < 3) continue;
-
-    results += calSeq(temp[1], false);
-  }
-
-  for (let key in hashmapZero) {
-    temp = hashmapZero[key];
-    if (temp < 3) continue;
-
-    results += calSeq(temp, true);
-  }
-
-  function calSeq(num, isZero) {
-    let total = 1;
-    let val = 2;
-    let zeroTotal = 1;
-
-    while (num > 3) {
-      total += val;
-      val++;
-      num--;
-
-      zeroTotal = zeroTotal * 2 + total;
-    }
-
-    if (isZero) return zeroTotal;
-    else return total;
-  }
-
-  return results;
+  return minCuts;
 };
 
-console.log(numberOfArithmeticSlices([2, 4, 6, 2, 8, 2, 3, 10, 2, 2, 2, 1, 0]));
+console.log(minCut("thannahehannaht"));
